6ebb1c35b3879483c4ac95afbfc6ea8b84ba7d29
CasparWilson <casparwilson111@gmail.com>
2024-08-05 15:12:36 +0100

    Got Langton's ant working

--- src/org/academy/langton/Ant.java
+++ src/org/academy/langton/Ant.java
@@ -41,12 +41,17 @@ public class Ant {
         // At a white square, turn 90° right, flip the color of the square, move forward one unit
         // At a black square, turn 90° left, flip the color of the square, move forward one unit
 
+        Cell currentCell = ground.cellAt(this.gridPosition);
+        if(currentCell.isActive()) {
+            turnCounterclockwise();
+        } else {
+            turnClockwise();
+        }
+        currentCell.toggleActive();
         moveForward();
+        }
 
         //Notes: In the ground object, "white" squares are "inactive", "black" squares are "active"
-
-
-    }
 
     private void moveForward() {
         moveInDirection(direction);
